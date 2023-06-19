e2895300df4ea859aed33625b7b152704b68486d
lisen6 <1789502923@qq.com>
2023-06-19 13:33:22 +0800

    feat: 重构了部分代码,支持CDN链接的方式安装

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
-# 使用方法如下
+# 使用方法
 
-![示例图片](./example.png)
+## 安装
+```js
+1. 下载此包
+2. 点开 chrome 浏览器扩展程序，到扩展程序页面
+3. 左上角有一个加载已解压的扩展程序，选中当前项目的文件夹即可
+```
+
+
+## 在浏览器控制台使用
+```js
+_.concat(1, [2, 3, 4])
+没有安装过 lodash 的话肯定会报错，ReferenceError: _ is not defined
+
+
+yarn.search('lodash')
+正在查询...
+lodash.js - https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js
+lowdb - https://cdnjs.cloudflare.com/ajax/libs/lowdb/1.0.0/low.min.js
+angular-filter - https://cdnjs.cloudflare.com/ajax/libs/angular-filter/0.5.17/angular-filter.min.js
+lodash-fp - https://cdnjs.cloudflare.com/ajax/libs/lodash-fp/0.10.4/lodash-fp.min.js
+rambdax - https://cdnjs.cloudflare.com/ajax/libs/rambdax/9.1.1/rambdax.umd.js
+alt-lodash - https://cdnjs.cloudflare.com/ajax/libs/alt-lodash/1.0.5/alt-lodash.esm.js
+lodash-compat - https://cdnjs.cloudflare.com/ajax/libs/lodash-compat/3.10.2/lodash.min.js
+
+// 该方法具有缓存功能，因为暂时没有支持版本号，所以安装同一个依赖但是不同版本号的时候，记得先刷新浏览器或者执行 yarn.clear()
+yarn('lodash.js')
+请稍等，正在安装包...
+安装完成 ✅ cdn: cdnjs - lodash.js@4.17.21 安装成功。
+安装时间：: 20.381103515625 ms
+
+
+_.concat(1, [2, 3, 4])
+(4) [1, 2, 3, 4]
+
+
+// 如果想要安装具体版本，比如 lodash，可使用上面 yarn.search('lodash') 搜索出来的具体 CDN 链接，手动替换版本号即可
+比如 yarn('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/0.1.0/lodash.min.js')
+
+
+// 打印安装过的依赖的信息
+yarn.log()
+
+
+// 清除所有安装过的依赖（刷新浏览器也可以）
+yarn.clear()
+```
--- content.js
+++ content.js
@@ -1,30 +1,60 @@
 class Yarn {
   constructor() {
     this.packages = []
-    this.registerCDN = [this.cdnjs, this.jsdelivr]
   }
 
   async add(packageName) {
-    if (this.checkPackageIsInstalled(packageName)) {
-      console.log(`${packageName} is already installed.`)
+    if (this.isURL(packageName)) {
+      const urlParams = this.extractPackageInfo(packageName)
+      const isInstalled = this.checkPackageIsInstalled(urlParams.packageName)
+      if (isInstalled) return console.log(`${urlParams.packageName} 已安装.`)
+      this.addYarn(urlParams)
       return
     }
 
-    for (let i = 0; i < this.registerCDN.length; i++) {
-      try {
-        const packageDetails = await this.registerCDN[i](packageName)
-        this.addYarn(packageDetails)
-        return
-      } catch (error) {
-        console.log(error)
-      }
+    const isInstalled = this.checkPackageIsInstalled(packageName)
+    if (isInstalled) return console.log(`${packageName} 已安装.`)
+
+    try {
+      const packageDetails = await this.cdnjs(packageName)
+      this.addYarn(packageDetails)
+      return
+    } catch (error) {
+      console.log(error, 123)
     }
 
-    this.fail(packageName)
+    this.fail(pkgName)
+  }
+
+  isURL(url) {
+    const urlRegex = /^https?:\/\//
+    return urlRegex.test(url)
+  }
+
+  extractPackageInfo(url) {
+    const [, packageName, version] =
+      url.match(
+        /https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*)\/(\d+\.\d+(?:\.\d+)?(?:-[a-zA-Z0-9]+)?)\/([a-zA-Z0-9-]+\.[a-zA-Z0-9]+)?/
+      ) || []
+
+    if (packageName && version) {
+      return {
+        packageName,
+        version,
+        cdn: 'cdnjs',
+        src: url
+      }
+    } else {
+      return null
+    }
+  }
+
+  isPackageInstalled(packageName) {
+    return this.checkPackageIsInstalled(packageName)
   }
 
   checkPackageIsInstalled(packageName) {
-    return this.packages.some((v) => v.packageName === packageName)
+    return this.packages.some(({ packageName: name }) => name === packageName)
   }
 
   async cdnjs(packageName) {
@@ -44,20 +74,36 @@ class Yarn {
     }
   }
 
-  async jsdelivr(packageName) {
-    const tempUrl = `https://cdn.jsdelivr.net/npm/${packageName}`
-    const access = await fetch(tempUrl)
-    if (access.ok) {
-      return { src: tempUrl, packageName, cdn: 'jsdelivr' }
+  // async jsdelivr(packageName) {
+  //   const tempUrl = `https://cdn.jsdelivr.net/npm/${packageName}`
+  //   const access = await fetch(tempUrl)
+  //   console.log(access, 'access')
+  //   if (access.ok) {
+  //     return { src: tempUrl, packageName, cdn: 'jsdelivr' }
+  //   }
+  //   throw new Error('Failed to fetch package from jsdelivr.')
+  // }
+
+  async getNpmLibrariesByCdnUrl(keyword, limit = 10) {
+    try {
+      const params = new URLSearchParams({
+        limit,
+        search: keyword
+      })
+      const url = `https://api.cdnjs.com/libraries?${params.toString()}`
+      const response = await fetch(url)
+      const data = await response.json()
+      if (!data.error) {
+        return data
+      }
+      throw new Error()
+    } catch (error) {
+      throw new Error()
     }
-    throw new Error('Failed to fetch package from jsdelivr.')
   }
 
   fail(packageName) {
-    const numWays = this.registerCDN.length
-    console.log(
-      `使用 ${numWays} 种方法加载，但未找到 ${packageName} 包。请检查包名称。`
-    )
+    console.log(`未找到 ${packageName} 包。请检查包名称。`)
   }
 
   addYarn(detail) {
@@ -81,12 +127,44 @@ class Yarn {
     document.body.appendChild(script)
   }
 
-  console() {
-    if (!this.packages.length) {
-      console.log('No packages found.')
+  log() {
+    const { packages } = this
+    if (!packages.length) {
+      console.log(`没有找到已安装的软件包.`)
       return
     }
-    console.table(this.packages)
+    console.table(packages)
+  }
+
+  async search(keyword, limit = 10) {
+    if (!keyword) {
+      throw new Error('请输入关键字搜索相应的NPM库.')
+    }
+
+    console.log('正在查询...')
+
+    const { results } = await this.getNpmLibrariesByCdnUrl(keyword, limit)
+
+    if(!results.length) {
+      return console.log(`未找到 ${keyword} 包.`)
+    }
+
+    results.forEach(({ name, latest }) => {
+      console.log(
+        `%c${name} - %c${latest}`,
+        'color: #00bfff;',
+        'color: #ff00ff;'
+      )
+    })
+  }
+
+  clear() {
+    this.packages = []
+    console.log(`Yarn 注册的所有包已清除`)
+  }
+
+  view(packageName) {
+
   }
 }
 
@@ -98,4 +176,11 @@ const yarn = function (packageName) {
     console.error(error)
   }
 }
-yarn.console = instance.console.bind(instance)
+yarn.log = instance.log.bind(instance)
+yarn.search = function (packageName) {
+  try {
+    instance.search(packageName)
+  } catch (error) {
+    console.error(error)
+  }
+}
